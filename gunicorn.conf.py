ce7cf3ad44e951486e0d03dfefdfbed54b7629fc
dev <asqarservis00001@gmail.com>
2026-09-16 15:10:03 +0500

    gunicorn runner add

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+import multiprocessing
+
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 4
+worker_class = "gthread"
+timeout = 120
+keepalive = 5
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
